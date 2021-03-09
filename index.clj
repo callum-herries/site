d8b07d9fcfc5c2deee9872d3a2bdcc0b5a24cce4
cal <>
2021-03-09 17:07:26 +0000

    Built css
    FossilOrigin-Name: 0e364008ee79886b197e413e55d0d0b56a74cfdd053e7956bcaffb442821f2d7

--- index.clj
+++ index.clj
@@ -1,15 +1,17 @@
 [:html
-  [:head
-   [:meta {:charset "UTF-8"}]
-   [:title "Callum Herries"]
-   [:meta {:name "description" :content "My personal site."}]
-   [:meta {:name "viewport" :content "width=device-width, initial-scale=1"}]
-   [:link {:rel "stylesheet" :href "resources/public/css/app-components.css"}]
-   [:link {:rel "stylesheet" :href "resources/public/css/app-utilities.css"}]
-   [:link {:rel "stylesheet" :href "https://fonts.googleapis.com/css?family=Roboto+Slab:700"}]
-   [:link {:rel "stylesheet" :href "https://fonts.googleapis.com/css?family=Crimson+Pro"}]]
+ [:head
+  [:meta {:charset "UTF-8"}]
+  [:title "Callum Herries"]
+  [:meta {:name "description" :content "My personal site."}]
+  [:meta {:name "viewport" :content "width=device-width, initial-scale=1"}]
+  [:link {:rel "stylesheet" :href "resources/public/css/app-components.css"}]
+  [:link {:rel "stylesheet" :href "resources/public/css/app-utilities.css"}]
+
+  [:link {:rel "stylesheet" :href "https://fonts.googleapis.com/css?family=Roboto+Slab:700"}]
+  [:link {:rel "stylesheet" :href "https://fonts.googleapis.com/css?family=Crimson+Pro"}]]
  [:body {:class "pt-24 p-8 leading-7 text-gray-700"}
   [:h1
    "Callum Herries"]
+  [:pre [:code]] ; Hack for postcss to not purge these elements code
   [:div {:class "my-3"}
    (markdown "intro.md")]]]
